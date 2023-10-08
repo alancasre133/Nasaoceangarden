482b87b34f8675f6bf48379b70ed58a08257f0cb
yeicob <jacobrdz34@gmail.com>
2023-10-07 22:34:06 -0600

    feat: improve model page layout

--- oceangarden/src/app/model/page.tsx
+++ oceangarden/src/app/model/page.tsx
@@ -1,13 +1,9 @@
-"use client";
+"use client"
 
-import Model from "@/components/Model3D/Model";
+import Model from "@/components/Model3D/Model"
 
 const Home = () => {
-  return (
-    <div style={{ width: "100%", height: "100%" }}>
-      <Model />
-    </div>
-  );
+  return <Model />
 }
 
-export default Home;
+export default Home
