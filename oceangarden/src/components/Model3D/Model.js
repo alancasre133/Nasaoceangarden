3261a0a2d9621ea1d6869da90b22f544e04a5aad
Hiram Castillo <hiramcastillo36@gmail.com>
2023-10-07 21:10:13 -0600

    fix: rename rocket component

--- oceangarden/src/components/Model3D/Model.js
+++ oceangarden/src/components/Model3D/Model.js
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from 'react';
 
 const Model = () => {
@@ -5,11 +6,15 @@ const Model = () => {
     const [num, setNum] = useState(0);
     const [pos, setPos] = useState("80deg 65deg 0deg");
 
+    const [pos2, setPos2] = useState("35deg 100deg 1deg");
+
+    const [pausar, setPausar] = useState(false);
   useEffect(() => {
     const modelViewer = document.querySelector('model-viewer');
 
     if (modelViewer) {
         modelViewer.setAttribute('camera-orbit', pos);
+        
       // Add a click event listener to the model-viewer component
       modelViewer.addEventListener('click', function (event) {
         // Handle the click event here
@@ -20,23 +25,40 @@ const Model = () => {
   }, []);
 
   const buttonPos = () => {
+    const modelViewer = document.querySelector('model-viewer');
     if(num == 0){
         setNum(1);
-        setPos("0deg 0deg 1deg");
+        setPos("80deg 65deg 0deg");
     }
     else {
         setNum(0);
-        setPos("0deg 0deg 0deg");
+        setPos("80deg 65deg 1deg");
     }
+    if(pausar == true){
+        modelViewer.removeAttribute('auto-rotate');
+        setPausar(false);
+    } else {
+        modelViewer.setAttribute('auto-rotate', 'true');
+        setPausar(true);
+    }
+    console.log(document.querySelector('model-viewer'));
   }
 
   const buttonPos2 = () => {
+    const modelViewer = document.querySelector('model-viewer');
     if(num == 0){
         setNum(1);
-        setPos("0deg 100deg 1deg");
+        setPos("35deg 100deg 1deg");
     } else {
         setPos("0deg 100deg 0deg");
         setNum(0);
+    }
+    if(pausar == true){
+        modelViewer.removeAttribute('auto-rotate');
+        setPausar(false);
+    } else {
+        modelViewer.setAttribute('auto-rotate', 'true');
+        setPausar(true);
     }
   }
 
