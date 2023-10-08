a6fc7f8c5f0a3b295d283092362f6940763d7567
Hiram Castillo <hiramcastillo36@gmail.com>
2023-10-07 22:51:38 -0600

    fix: model positions

--- oceangarden/src/components/Model3D/Model.js
+++ oceangarden/src/components/Model3D/Model.js
@@ -1,97 +1,121 @@
-"use client"
-import React, { useEffect, useState } from "react"
+"use client";
+import React, { useEffect, useState } from 'react';
 
 const Model = () => {
-  const [num, setNum] = useState(0)
-  const [pos, setPos] = useState("80deg 65deg 0deg")
 
-  const [pos2, setPos2] = useState("35deg 100deg 1deg")
+    const [num, setNum] = useState(0);
+    const [pos, setPos] = useState("80deg 65deg 0deg");
 
-  const [pausar, setPausar] = useState(false)
+    const [pos2, setPos2] = useState("35deg 100deg 1deg");
+
+    const [pausar, setPausar] = useState(true);
+
   useEffect(() => {
-    const modelViewer = document.querySelector("model-viewer")
+    const modelViewer = document.querySelector('model-viewer');
 
     if (modelViewer) {
-      modelViewer.setAttribute("camera-orbit", pos)
-
+        modelViewer.setAttribute('camera-orbit', pos);
+        
       // Add a click event listener to the model-viewer component
-      modelViewer.addEventListener("click", function (event) {
+      modelViewer.addEventListener('click', function (event) {
         // Handle the click event here
-        console.log("Model clicked!", event)
+        console.log('Model clicked!', event);
         // You can perform any action or interaction you want here
-      })
+      });
     }
-  }, [])
+  }, []);
 
   const buttonPos = () => {
-    const modelViewer = document.querySelector("model-viewer")
-    if (num == 0) {
-      setNum(1)
-      setPos("80deg 65deg 0deg")
-    } else {
-      setNum(0)
-      setPos("80deg 65deg 1deg")
+    console.log("hola");
+    const modelViewer = document.querySelector('model-viewer');
+    if(num == 0){
+        setNum(1);
+        setPos("80deg 80deg 0deg");
     }
-    if (pausar == true) {
-      modelViewer.removeAttribute("auto-rotate")
-      setPausar(false)
-    } else {
-      modelViewer.setAttribute("auto-rotate", "true")
-      setPausar(true)
+    else {
+        setNum(0);
+        setPos("80deg 80deg 1deg");
     }
-    console.log(document.querySelector("model-viewer"))
+    if(pausar === true){
+        modelViewer.removeAttribute('auto-rotate');
+        setPausar(false);
+    } 
+    
   }
 
   const buttonPos2 = () => {
-    const modelViewer = document.querySelector("model-viewer")
-    if (num == 0) {
-      setNum(1)
-      setPos("35deg 100deg 1deg")
+    console.log("hola");
+    const modelViewer = document.querySelector('model-viewer');
+    if(num == 0){
+        setNum(1);
+        setPos("135deg 75deg 1deg");
     } else {
-      setPos("0deg 100deg 0deg")
-      setNum(0)
+        setPos("135deg 75deg 0deg");
+        setNum(0);
     }
-    if (pausar == true) {
-      modelViewer.removeAttribute("auto-rotate")
-      setPausar(false)
+    if(pausar === true){
+        modelViewer.removeAttribute('auto-rotate');
+        setPausar(false);
+    } 
+   
+  }
+
+  const buttonPos3 = () => {
+    console.log("hola");
+    const modelViewer = document.querySelector('model-viewer');
+    if(num == 0){
+        setNum(1);
+        setPos("620deg 90deg 100deg");
     } else {
-      modelViewer.setAttribute("auto-rotate", "true")
-      setPausar(true)
+        setPos("620deg 90deg 101deg");
+        setNum(0);
     }
+    if(pausar === true){
+        modelViewer.removeAttribute('auto-rotate');
+        setPausar(false);
+    } 
+   
   }
 
   return (
     <div>
-      <button onClick={buttonPos}>Pacifico</button>
-      <button onClick={buttonPos2}>Atlantico</button>
-      <button onClick={buttonPos2}>Indico</button>
-      <div style={{ width: "100vw", height: "100vh" }}>
-        <model-viewer
-          src="earth.glb"
-          alt="A 3D model of an astronaut"
-          auto-rotate
-          ar
-          ar-modes="webxr scene-viewer"
-          style={{
-            width: "100%",
-            height: "100%",
-            backgroundColor: "transparent",
-          }}
-          shadow-intensity="10"
-          rotation-per-second="0.0001"
-          camera-controls // Utiliza solo los controles de órbita
-          camera-orbit={pos}
-          brightness="1.5"
-          disable-pan
-        ></model-viewer>
-        <script
-          async
-          type="module"
-          src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"
-        ></script>
-      </div>
+        <button onClick={buttonPos}
+            > 
+            Pacifico
+        </button>
+        <button onClick={buttonPos2}
+            > 
+            Atlantico
+        </button>
+        <button onClick={buttonPos3}
+            > 
+            Indico
+        </button>
+        <div style={{ width: "100vw", height: "100vh" }}>
+            <model-viewer
+                src="earth.glb"
+                alt="A 3D model of an astronaut"
+                auto-rotate
+                ar 
+                style={{ width: "100%", height: "100%", backgroundColor: "transparent"}}
+                shadow-intensity="10"
+                camera-controls // Utiliza solo los controles de órbita
+                camera-orbit={pos}
+                brightness="1.5"
+                disable-pan
+            >
+                
+            </model-viewer>
+            <script
+                async
+                type="module"
+                src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"
+            ></script>
+        </div>
     </div>
-  )
-}
+    
+  );
+};
 
-export default Model
+export default Model;
+    
