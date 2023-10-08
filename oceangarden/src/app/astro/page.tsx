0e9c42f7d912e285cce82b02cd3f5ce42bb64868
Hiram Castillo <hiramcastillo36@gmail.com>
2023-10-07 22:51:47 -0600

    feat: add astro model

---
+++ oceangarden/src/app/astro/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Astro from "@/components/Model3D/Astro";
+
+const Home = () => {
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <Astro />
+    </div>
+  );
+}
+
+export default Home;
