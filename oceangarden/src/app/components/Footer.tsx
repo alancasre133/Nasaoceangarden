01643d9314cf868656560b2c03a1bd298588ee62
yeicob <jacobrdz34@gmail.com>
2023-10-08 08:56:54 -0600

    refactor: render links in footer

--- oceangarden/src/app/components/Footer.tsx
+++ oceangarden/src/app/components/Footer.tsx
@@ -1,19 +1,10 @@
 "use client"
 import Link from "next/link"
 import { useState } from "react"
-import { ELEMENTS } from "../constants"
-import { usePathname } from "next/navigation"
+import { useSectionLinks } from "../hooks/useSectionLinks"
 
 export function Footer() {
-  const pathname = usePathname()
-
-  const FOOTER_LINKS = Object.values(ELEMENTS).map(
-    ({ id, linkText, soundSrc }) => ({
-      href: `${pathname}?modal=${id}`,
-      text: linkText,
-      sound: soundSrc,
-    }),
-  )
+  const { links } = useSectionLinks()
 
   const [isAudioPlaying, setIsAudioPlaying] = useState(false)
   const [audio, setAudio] = useState<HTMLAudioElement | null>(null)
@@ -35,9 +26,9 @@ export function Footer() {
   }
 
   return (
-    <footer className="z-10 w-full pt-4">
+    <footer className="z-10 w-full pt-16">
       <ul className="flex flex-col gap-x-28 md:flex-row items-center justify-center flex-wrap gap-y-1">
-        {FOOTER_LINKS.map(({ href, text, sound }) => (
+        {links.map(({ href, text, sound }) => (
           <li key={href}>
             <Link
               href={href}
