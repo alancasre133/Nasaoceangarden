19add3bea3fdec74090c655c83175b697bce2de8
yeicob <jacobrdz34@gmail.com>
2023-10-08 08:58:25 -0600

    feat: generate header conditionally

--- oceangarden/src/app/components/Header.tsx
+++ oceangarden/src/app/components/Header.tsx
@@ -2,8 +2,27 @@
 
 import { useState } from "react"
 import { Logo } from "./Logo"
-import { Headphones, HeadphonesOff } from "./Icons"
+import { LeftArrowIcon, Volume, VolumeOff } from "./Icons"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { useSectionLinks } from "../hooks/useSectionLinks"
+
+const ROOT_PATH = "/"
+
+const isRootPath = (pathname: string) => pathname === ROOT_PATH
+
+const HomeLinkContent = () => {
+  // const pathname = usePathname()
+
+  return <Logo />
+
+  // return (
+  //   <>
+  //     <LeftArrowIcon />
+  //     <span className="font-medium whitespace-nowrap">Home</span>
+  //   </>
+  // )
+}
 
 export function Header() {
   const [isAudioPlaying, setIsAudioPlaying] = useState(false)
@@ -12,16 +31,35 @@ export function Header() {
     setIsAudioPlaying((state) => !state)
   }
 
+  const pathname = usePathname()
+  const { bareLinks } = useSectionLinks()
+
+  const isRoot = pathname === ROOT_PATH
+
   return (
-    <header className="flex gap-4 justify-between items-center w-full py-4 z-10">
-      <Link href="/">
-        <Logo />
+    <header className="flex gap-16 justify-between items-center w-full py-4 z-50 text-lg">
+      <Link href="/" className="flex gap-4 items-center hover:text-slate-300">
+        <HomeLinkContent />
       </Link>
+
+      <nav className="w-full">
+        <ul className="flex items-center justify-end gap-11 list-none">
+          {isRoot === false &&
+            bareLinks.map(({ href, text }) => (
+              <li key={href}>
+                <Link href={href} className="hover:underline">
+                  {text}
+                </Link>
+              </li>
+            ))}
+        </ul>
+      </nav>
+
       <button
         onClick={toggleAudio}
         className="flex items-center justify-center"
       >
-        {isAudioPlaying ? <HeadphonesOff /> : <Headphones />}
+        {isAudioPlaying ? <VolumeOff /> : <Volume />}
       </button>
     </header>
   )
